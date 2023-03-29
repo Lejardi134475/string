771d618291c737f92509533fd040801941b9e1a1
Lejardi134475 <128643562+Lejardi134475@users.noreply.github.com>
2023-03-29 17:39:35 -0400

    Add files via upload

---
+++ holaMiNombreEs.cpp
@@ -0,0 +1,16 @@
+// Computadora te saluda a ti
+//Autor: Gustavo Lejardi
+
+#include <iostream>
+
+int main()
+{
+     string colega;
+
+	cout << "Como te llamas?";
+	cin >> colega;
+
+	cout << "Hola " << colega << "como estas?";
+
+	return 0;
+}
